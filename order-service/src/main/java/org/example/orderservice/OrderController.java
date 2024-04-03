b2cb72a997c295c1fb7f834773e51b9affe323b6
user <user@mail.ru>
2024-04-04 00:15:05 +0300

    Grafana Tempo (Traces) — Loki (Logs)

--- order-service/src/main/java/org/example/orderservice/OrderController.java
+++ order-service/src/main/java/org/example/orderservice/OrderController.java
@@ -1,11 +1,13 @@
 package org.example.orderservice;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/orders")
 @RequiredArgsConstructor
@@ -15,6 +17,7 @@ public class OrderController {
 
     @GetMapping("/{id}")
     public Order findById(@PathVariable Long id) {
+        log.info("Find order by id {}", id);
         return this.orderRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid id: %d".formatted(id)));
     }
